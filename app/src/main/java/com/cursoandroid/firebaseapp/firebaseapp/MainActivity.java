3f6425f2a99d0f80ae103e943ae5dcd997ae6234
Carlos Bruno <cbcarlos08@gmail.com>
2017-05-31 13:01:57 -0400

    Curso de Android

--- app/src/main/java/com/cursoandroid/firebaseapp/firebaseapp/MainActivity.java
+++ app/src/main/java/com/cursoandroid/firebaseapp/firebaseapp/MainActivity.java
@@ -3,11 +3,20 @@ package com.cursoandroid.firebaseapp.firebaseapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+
+    private DatabaseReference firebaseDatabase = FirebaseDatabase.getInstance().getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        firebaseDatabase.child("pontos").setValue("100");
+
+
     }
 }
